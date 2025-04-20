d0e901d0cceeadcb14fee2f6b234041d3e7f6104
Milani <milaniwhite12@gmail.com>
2025-04-20 15:46:05 -0400

    committing whole capstone project

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ if (JWT_SECRET === "shhh") {
 
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL ||  'postgres://postgres:milaniwhite@localhost/career_simulation/',
+  //connectionString: process.env.DATABASE_URL ||  'postgres://postgres:milaniwhite@localhost/career_simulation/',
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
